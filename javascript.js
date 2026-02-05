f3db2a1b789522197b2573279979e4bc7c6a294a
Tuva Luva <235779229+Tuva-Luva@users.noreply.github.com>
2026-02-05 21:01:34 +0100

    Add pseudocode for human choice

--- javascript.js
+++ javascript.js
@@ -4,25 +4,33 @@
   1. Create a function that outputs a random number between 0 and 1 (and a varible to store that output.  
   2. Translate that number to rock, papper or scissor. 
   3. Save the choice in a varible?
+
+X Humans choice: 
+ 1. Create a function that lets the user write a valid alternative for RPS.
+ 2. use if/else if. 
+ 3. Create a varible to store the users input in so that it (later) can be compared to the computers output. 
 */
 
-let randomNumber = Math.random();
-
 function getComputerChoice() {
- if (randomNumber <= 0.333)  {
-  let rock = "Rock";
-  return rock;
- }
+  let randomNumber = Math.random();
 
- else if (randomNumber > 0.333 && randomNumber <= 0.666) {
-  let paper = "Paper";
-  return paper;
- } 
+  if (randomNumber <= 0.333)  {
+    let rock = "Rock";
+    return rock;
+  }
 
- else if (randomNumber > 0.666) {
-  let scissor = "Scissor";
-  return scissor;
- }
+  else if (randomNumber > 0.333 && randomNumber <= 0.666) {
+    let paper = "Paper";
+    return paper;
+  } 
+
+  else if (randomNumber > 0.666) {
+    let scissor = "Scissor";
+    return scissor;
+  }
 }
 
 console.log(getComputerChoice())
+
+function getHumanChoice() {}
+
